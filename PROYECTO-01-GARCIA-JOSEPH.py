037f527b4fbc4993e4fb34f81f230b09fe7e6d65
tongggggo <107387050+tongggggo@users.noreply.github.com>
2022-08-19 17:25:50 -0600

    Update PROYECTO-01-GARCIA-JOSEPH.py

--- PROYECTO-01-GARCIA-JOSEPH.py
+++ PROYECTO-01-GARCIA-JOSEPH.py
@@ -16,3 +16,22 @@ with open('synergy_logistics_database.csv') as archivo_csv:
    
     res = Counter(ciudades)
     print(res)
+"""    
+Synergy logistics está considerando la posibilidad de enfocar sus esfuerzos en las
+10 rutas más demandadas. Acorde a los flujos tanto de importación como de
+exportación, indica:
+
+● ¿Cuáles son esas 10 rutas?
+    Corea del sur - Vietnam (497)
+    Países bajos - Belgica (437)
+    Estados Unidos - Países bajos (385)
+    China - Mexico (351)
+    China - Japon (343)
+    Alemania - China (328)
+    Japon - Francia (299)
+    Corea del sur - Japon (294)
+    Australia - Singapore (273)
+    Japon - Canadá (273)
+● ¿Le conviene implementar esa estrategia? ¿Por qué? 
+    Creo que sí, aunque existen algunos riesgos; como por ejemplo el que las rutas menos utilizadas empiezen a funcionar aún peor
+    y que los cambios en sus rutas más utilizadas no sean lo suficientemente grandes como para compensarlo
